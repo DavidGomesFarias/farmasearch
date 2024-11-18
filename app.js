7530b47ad135ef1f2cdd6970e0b9da6f3208c855
David Gomes <98854034+DavidGomesFarias@users.noreply.github.com>
2024-11-17 23:42:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'fariasdi_david',
+  password: 'Davidgomes@123',
+  database: 'fariasdi_davidDB'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Conectado ao MySQL');
+});
+
+// // Fixo
+// app.get('/dados', (req, res) => {
+//   console.log('Requisição recebida para /dados');
+//   db.query('SELECT * FROM anapolis', (err, results) => {
+//     if (err) {
+//       console.error('Erro na consulta:', err);
+//       return res.status(500).json({ error: 'Erro no servidor' });
+//     }
+//     res.json(results);
+//   });
+// });
+
+
+
+  // Funções CRUD
+app.get('/dados/:cidade', (req, res) => {
+  const cidade = req.params.cidade;
+
+  db.query(`SELECT * FROM ${cidade}`, (err, results) => {
+    if (err) {
+      console.error('Erro na consulta:', err);
+      return res.status(500).json({ error: 'Erro no servidor' });
+    }
+
+    // Formatação das datas antes de enviar ao front-end
+    const formattedResults = results.map(item => {
+      if (item.data_pedido) {
+        item.data_pedido = new Date(item.data_pedido).toLocaleDateString('pt-BR', { timeZone: 'UTC' });
+      }
+      if (item.data_previsao) {
+        item.data_previsao = new Date(item.data_previsao).toLocaleDateString('pt-BR', { timeZone: 'UTC' });
+      }
+      return item;
+    });
+
+    res.json(formattedResults);
+  });
+});
+
+
+
+// // Funções CRUD
+// app.get('/dados', (req, res) => {
+//   db.query(`SELECT * FROM goiania`, (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
+
+// app.post('/dados', (req, res) => {
+//   const newItem = { text: req.body.text };
+//   db.query('INSERT INTO itens SET ?', newItem, (err, result) => {
+//     if (err) throw err;
+//     res.sendStatus(201);
+//   });
+// });
+
+// app.put('/dados/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { text } = req.body;
+//   db.query('UPDATE itens SET text = ? WHERE id = ?', [text, id], (err, result) => {
+//     if (err) throw err;
+//     res.sendStatus(200);
+//   });
+// });
+
+// app.delete('/dados/:id', (req, res) => {
+//   const { id } = req.params;
+//   db.query('DELETE FROM itens WHERE id = ?', [id], (err, result) => {
+//     if (err) throw err;
+//     res.sendStatus(200);
+//   });
+// });
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
---
+++ index.js
@@ -0,0 +1,108 @@
+function updateItemList(cidade) {
+  const cidadeFormatada = cidade.replace(/\s+/g, '')
+  fetch(`http://localhost:3000/dados/${cidadeFormatada}`) // Ajuste a URL conforme necessário
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.statusText}`);
+      }
+      return response.json(); // Converte a resposta para JSON
+    })
+    .then(data => {
+      console.log('Dados recebidos:', data); // Verifique a estrutura
+      const itemList = document.querySelector('#itemList');
+      itemList.innerHTML = ''; // Limpar a lista existente
+      data.forEach(item => {
+        const li = document.createElement('li');
+        const pDisponibilidade  = document.createElement('p');
+        const pDataPedido  = document.createElement('p');
+        const pDataPrevisao  = document.createElement('p');
+        pDisponibilidade.textContent = item.disponibilidade || 'Sem informações'
+        pDataPedido.textContent = `Data do pedido: ${item.data_pedido}` || 'Sem informações'
+        pDataPrevisao.textContent = `Previsão de chegada: ${item.data_previsao}`  || 'Sem informações'
+        if (item.disponibilidade === 'Disponível') {
+          pDisponibilidade.classList.add('disponivel')
+          pDataPedido.style.display = 'none'
+          pDataPrevisao.style.display = 'none'
+          console.log(`Item ${item.nome_remedio} está disponível.`);
+            } else if (item.disponibilidade === 'Indisponível') {
+              pDisponibilidade.classList.add('indisponivel')
+              console.log(`Item ${item.nome_remedio} está indisponível.`);
+            } else {
+        // Caso a disponibilidade tenha outro valor ou seja nula
+        console.log(`Item ${item.nome_remedio} tem status desconhecido.`);
+      }
+        li.textContent = item.nome_remedio || 'Item sem texto'; // Defina um texto padrão se 'nome_remedio' for indefinido
+        li.dataset.id = item.id || '0'; // Defina um ID padrão se 'id' for indefinido
+        itemList.appendChild(li);
+        li.appendChild(pDisponibilidade)
+        li.appendChild(pDataPedido)
+        li.appendChild(pDataPrevisao)
+      });
+
+      document.getElementById('searchInput2').addEventListener('keyup', function() {
+        const input = this.value.toLowerCase();
+        const items = itemList.querySelectorAll('li');
+
+
+        // Os itens comentados são para fala que não achou nenhum remedio caso o usuário escreva algum remédio não encontrado.
+
+        
+        // let found = false;
+        // const itemList2 = document.querySelector('#itemList');
+        // const nenhumRemedio = document.createElement('p')
+        // nenhumRemedio.textContent = 'Nenhum remédio foi encontrado com esse nome!'
+        items.forEach(function(item) {
+          const text = item.textContent.toLowerCase();
+          if (text.includes(input)) {
+            item.style.display = 'flex';
+            // found = true;
+          } else {
+            item.style.display = 'none';
+            // found = false;
+          }
+
+          // if(found == false) {
+          //   itemList.appendChild(nenhumRemedio);
+          // }
+        });
+      });
+    })
+    .catch(error => console.error('Erro:', error));
+}
+
+document.getElementById('searchInput').addEventListener('keyup', function() {
+  const input = this.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Pega o valor do input e converte para minúsculas
+  const items = document.querySelectorAll('.itemList li'); // Seleciona todos os itens da lista
+
+  items.forEach(function(item) { 
+    const text = item.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Pega o texto de cada item e converte para minúsculas
+    if (text.includes(input)) { // Verifica se o texto do item contém o que foi digitado
+      item.style.display = 'block'; // Mostra o item se ele contiver o texto
+    } else {
+      item.style.display = 'none'; // Esconde o item se não contiver o texto
+    }
+  });
+
+  items.forEach(function(item) {
+    item.addEventListener('click', () => {
+      items.forEach(function(i) {
+        i.style.display = 'none';
+      });
+
+      const textNormal = item.textContent;
+      let cidade = item.textContent;
+      cidade = cidade.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+      const main = document.querySelector('.main');
+      main.innerHTML = `<div class="containerMain">
+        <h2>${textNormal}</h2>
+        <input type="text" id="searchInput2" placeholder="Buscar Remédio">
+        <ul id="itemList" style="display: flex;">
+
+        </ul>
+      </div>`;
+      
+      updateItemList(cidade)
+      document.getElementById('searchInput').value = textNormal;
+    });
+  });
+});
